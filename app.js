b534f4d38fb81f39a16c5debaca16f5f480976fa
Steve Calebrese <stevecalebrese@gmail.com>
2022-02-16 22:08:11 -0500

    html output

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const profileDateArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDateArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
 
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name ="viewport" content="width=device=width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+    </head>
 
-const printProfileData = (profileDataArr) => {
-    for (let i=0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-
-    profileDataArr.forEach((profileItem)=> console.log(profileItem));
+    <body>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
 };
 
-///make the same but written differently
-
-
-
-printProfileData(profileDateArgs);
+console.log(generatePage(name, github));
