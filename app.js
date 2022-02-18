57de3904abd0756d7d1f76e592acd9da4f8e30fb
Steve Calebrese <stevecalebrese@gmail.com>
2022-02-17 21:22:43 -0500

    inquirer

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-//must define fs for it to be accessible...this is for efficiency purposes so every file doesn't load entire library
-const fs = require('fs');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-const [name, github] = profileDataArgs;
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-  
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
